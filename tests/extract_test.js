aa32e627153d91ed3c20ba8a68afecd1a1c8b079
Luan Phan <luanphandinh@outlook.com>
2020-01-21 11:00:35 +0700

    dependOn > unit tests: make sure that provide correct pit suite structure

--- tests/extract_test.js
+++ tests/extract_test.js
@@ -2,14 +2,14 @@ const { getSuites } = require("../lib/extract");
 const assert = require("./assert");
 
 const path = require("path");
-suites = getSuites(path.join(__dirname, "suites"));
+suites = getSuites(path.join(__dirname, "suites"), "fun_suite");
 
 const expected = {
   suite: {
     name: "Suite name",
     description: "Here go the suite description",
     callback:
-      '() => {\n  sendRequest({\n    method: "GET",\n    url: {\n      raw: "https://request_something"\n    }\n  });\n\n  before("200", () => {});\n\n  test("200", "Test 200 description", () => {});\n\n  before("404", () => {});\n\n  test("404", "Test 404", () => {});\n}'
+      '() => {\n  sendRequest({\n    method: "GET",\n    url: {\n      raw: "https://request_something"\n    }\n  });\n\n  dependOn("Have dependencies suite");\n\n  before("200", () => {});\n\n  test("200", "Test 200 description", () => {});\n\n  before("404", () => {});\n\n  test("404", "Test 404", () => {});\n}'
   },
   tests: {
     "200": {
@@ -28,7 +28,7 @@ const expected = {
   schedule: ["200", "404"],
   lock: true,
   request: { method: "GET", url: { raw: "https://request_something" } },
-  depend: []
+  depend: ["Have dependencies suite"]
 };
 
 assert.Equal(JSON.stringify([expected]), JSON.stringify(suites));
--- tests/t_test.js
+++ tests/t_test.js
@@ -1,6 +1,12 @@
-const { describe, before, test, runTest, sendRequest } = require("../lib/t")(
-  true
-);
+const {
+  describe,
+  before,
+  test,
+  runTest,
+  dependOn,
+  sendRequest
+} = require("../lib/t")(true);
+
 const assert = require("./assert");
 
 const actual = describe("Suite name", "Here go the suite description", () => {
@@ -10,6 +16,8 @@ const actual = describe("Suite name", "Here go the suite description", () => {
       raw: "https://request_something"
     }
   });
+
+  dependOn("Have dependencies suite");
 
   before("200", () => {});
 
@@ -25,7 +33,7 @@ const expected = {
     name: "Suite name",
     description: "Here go the suite description",
     callback:
-      '() => {\n  sendRequest({\n    method: "GET",\n    url: {\n      raw: "https://request_something"\n    }\n  });\n\n  before("200", () => {});\n\n  test("200", "Test 200 description", () => {});\n\n  before("404", () => {});\n\n  test("404", "Test 404", () => {});\n}'
+      '() => {\n  sendRequest({\n    method: "GET",\n    url: {\n      raw: "https://request_something"\n    }\n  });\n\n  dependOn("Have dependencies suite");\n\n  before("200", () => {});\n\n  test("200", "Test 200 description", () => {});\n\n  before("404", () => {});\n\n  test("404", "Test 404", () => {});\n}'
   },
   tests: {
     "200": {
@@ -44,7 +52,7 @@ const expected = {
   schedule: ["200", "404"],
   lock: true,
   request: { method: "GET", url: { raw: "https://request_something" } },
-  depend: []
+  depend: ["Have dependencies suite"]
 };
 
 assert.Equal(JSON.stringify(expected), JSON.stringify(actual));
