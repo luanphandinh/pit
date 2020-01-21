2be69c4bc9fdb7e5722959e962ed793694af9121
Luan Phan <luanphandinh@outlook.com>
2020-01-21 21:53:35 +0700

    lib/extract > tests

--- tests/extract_test.js
+++ tests/extract_test.js
@@ -1,34 +1,81 @@
-const { getSuites } = require("../lib/extract");
+const { extractCollection } = require("../lib/extract");
 const assert = require("./assert");
-
 const path = require("path");
-suites = getSuites(path.join(__dirname, "suites"), "fun_suite");
 
-const expected = {
-  suite: {
-    name: "Suite name",
-    description: "Here go the suite description",
-    callback:
-      '() => {\n  sendRequest({\n    method: "GET",\n    url: {\n      raw: "https://request_something"\n    }\n  });\n\n  dependOn("Have dependencies suite");\n\n  before("200", () => {});\n\n  test("200", "Test 200 description", () => {});\n\n  before("404", () => {});\n\n  test("404", "Test 404", () => {});\n}'
-  },
-  tests: {
-    "200": {
-      before: "() => {}",
-      name: "200",
-      description: "Test 200 description",
-      callback: "() => {}"
-    },
-    "404": {
-      before: "() => {}",
-      name: "404",
-      description: "Test 404",
-      callback: "() => {}"
+const collection = extractCollection(path.join(__dirname, "suites"));
+
+const funSuiteItem = {
+  name: "Suite name",
+  item: [
+    {
+      name: "Suite name",
+      request: {
+        method: "GET",
+        header: [],
+        url: {
+          raw: "https://request_something"
+        }
+      },
+      event: [
+        {
+          listen: "test",
+          script: {
+            exec: [
+              'const { runTest } = eval(pm.variables.get("t"))();\nrunTest();'
+            ]
+          }
+        },
+        {
+          listen: "prerequest",
+          script: {
+            exec: [
+              'const { describe, before, test, sendRequest } = eval(pm.variables.get("t"))();\n',
+              'describe("Suite name", "Here go the suite description", () => {\n  sendRequest({\n    method: "GET",\n    url: {\n      raw: "https://request_something"\n    }\n  });\n\n  dependOn("Have dependencies suite");\n\n  before("200", () => {});\n\n  test("200", "Test 200 description", () => {});\n\n  before("404", () => {});\n\n  test("404", "Test 404", () => {});\n});'
+            ]
+          }
+        }
+      ]
     }
-  },
-  schedule: ["200", "404"],
-  lock: true,
-  request: { method: "GET", url: { raw: "https://request_something" } },
-  depend: ["Have dependencies suite"]
+  ]
 };
 
-assert.Equal(JSON.stringify([expected]), JSON.stringify(suites));
+const haveDependenciesSuite = {
+  name: "Have dependencies suite",
+  item: [
+    {
+      name: "Have dependencies suite",
+      request: {
+        method: "GET",
+        header: [],
+        url: {
+          raw: "{{request_url}}",
+          host: ["{{request_url}}"]
+        }
+      },
+      event: [
+        {
+          listen: "test",
+          script: {
+            exec: [
+              'const { runTest } = eval(pm.variables.get("t"))();\nrunTest();'
+            ]
+          }
+        },
+        {
+          listen: "prerequest",
+          script: {
+            exec: [
+              'const { describe, before, test, sendRequest } = eval(pm.variables.get("t"))();\n',
+              'describe("Have dependencies suite", "", () => {\n  test("401", "Test 401 description");\n});'
+            ]
+          }
+        }
+      ]
+    }
+  ]
+};
+
+assert.Equal(
+  JSON.stringify([funSuiteItem, haveDependenciesSuite]),
+  JSON.stringify(collection.item)
+);
