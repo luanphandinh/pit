3251642878eb16c0c7db5b8a82b0ac9f6295afe7
Luan Phan <luanphandinh@outlook.com>
2020-01-24 15:13:22 +0700

    extract collection with config

--- example/collection.js
+++ example/collection.js
@@ -1,5 +1,16 @@
 const { extractCollection } = require("../lib/extract");
 const path = require("path");
 
-const collection = extractCollection(path.join(__dirname));
+const collection = extractCollection({
+  info: {
+    name: "Example Fun Postman",
+    schema:
+      "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+  },
+  suites: {
+    includes: ["_suite"],
+    paths: [path.join(__dirname)]
+  }
+});
+
 console.log(JSON.stringify(collection, null, 4));
--- tests/extract_test.js
+++ tests/extract_test.js
@@ -2,7 +2,17 @@ const { extractCollection } = require("../lib/extract");
 const assert = require("./assert");
 const path = require("path");
 
-const collection = extractCollection(path.join(__dirname, "suites"));
+const collection = extractCollection({
+  info: {
+    name: "Example Fun Postman",
+    schema:
+      "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
+  },
+  suites: {
+    includes: ["_suite"],
+    paths: [path.join(__dirname, "suites")]
+  }
+});
 
 const funSuiteItem = {
   name: "Suite name",
